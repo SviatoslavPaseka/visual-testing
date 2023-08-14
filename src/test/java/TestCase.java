a5acc5ded2428af3de0700ba66fd014af1d76cb3
SviatoslavPaseka <paseka.syat@gmail.com>
2023-08-14 14:52:58 +0300

    first commit

--- src/test/java/TestCase.java
+++ src/test/java/TestCase.java
@@ -1,44 +1,89 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
+import com.applitools.eyes.*;
+import com.applitools.eyes.selenium.*;
+import com.applitools.eyes.selenium.fluent.Target;
+import com.applitools.eyes.visualgrid.services.RunnerOptions;
+import com.applitools.eyes.visualgrid.services.VisualGridRunner;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class TestCase {
-    WebDriver driver;
+    static ChromeDriver driver;
+    static BatchInfo myBatch;
+    static Configuration suiteConfiguration;
+    static EyesRunner eyesRunner;
+    Eyes eyes;
+    @BeforeAll
+    public static void beforeAll(){
+        myBatch = new BatchInfo("My First Test");
+        myBatch.setSequenceName("Advanced visual testing");
+        myBatch.setNotifyOnCompletion(true);
+        ChromeOptions options = new ChromeOptions();
+        options.setBinary("/Applications/Google Chrome.app/Contents/MacOS/Google Chrome");
+        driver = new ChromeDriver(options);
+        suiteConfiguration = new Configuration();
+        suiteConfiguration.setApiKey("gs7u1018MIbLDJ4ENFd9d101NdIeKZ0f6rsUaYNcSWQbNIQ110");
+        suiteConfiguration.setBatch(myBatch);
+//        suiteConfiguration.setMatchLevel(MatchLevel.LAYOUT);
+        suiteConfiguration.addBrowser(1000, 600, BrowserType.CHROME);
+//        suiteConfiguration.setStitchMode(StitchMode.CSS);
+        eyesRunner = new VisualGridRunner(new RunnerOptions().testConcurrency(5));
+//        eyesRunner = new ClassicRunner();
+    }
 
     @BeforeEach
-    public void beforeEach() {
-        driver = WebDriverManager.chromedriver().create();
+    public void beforeEach(TestInfo testInfo) {
+//        System.setProperty("webdriver.chrome.driver", "/Users/sviatoslavpaseka/Downloads/chromedriver-115/chromedriver");
+        eyes = new Eyes(eyesRunner);
+        eyes.setConfiguration(suiteConfiguration);
+        eyes.setBatch(myBatch);
+        eyes.setApiKey("gs7u1018MIbLDJ4ENFd9d101NdIeKZ0f6rsUaYNcSWQbNIQ110");
+//        driver.manage().window().maximize();
+        eyes.open(driver,
+                "First test case",
+                testInfo.getTestMethod().get().getName(),
+                new RectangleSize(1510, 754));
     }
     @Test
     public void myTestCase() {
-        driver.get("https://applitools.com/helloworld/");
-        WebElement numbers = driver.findElement(By.cssSelector("span.primary"));
-        WebElement button = driver.findElement(By.cssSelector("div.section:nth-child(3) > button:nth-child(1)"));
-        WebElement titleH = driver.findElement(By.cssSelector("div.fancy:nth-child(1) > span:nth-child(1)"));
-        WebElement titleD = driver.findElement(By.cssSelector("div.fancy:nth-child(1) > span:nth-child(11)"));
+        driver.get("https://applitools.com/helloworld/?diff1");
+        eyes.check(Target.window());
+    }
 
-        Assertions.assertEquals(numbers.isDisplayed(), true);
-        Assertions.assertEquals(numbers.getText(), "123456");
-        Assertions.assertEquals(numbers.getCssValue("color"), "rgba(78, 90, 99, 1)");
 
-        Assertions.assertEquals(button.isDisplayed(), true);
-        Assertions.assertEquals(button.getText(), "Click me!");
-        Assertions.assertEquals(button.getCssValue("color"), "rgba(255, 255, 255, 1)");
+    @Test
+    public void applitoolsTutorialPageViewPort() {
+        driver.get("https://applitools.com/tutorials/quickstart/web/selenium/java/junit");
+        eyes.check(Target.region(By.cssSelector(".navbar")).fully());
+//        eyes.check(Target.window().fully(false));
+    }
 
-        Assertions.assertEquals(titleH.isDisplayed(), true);
-        Assertions.assertEquals(titleH.getText(), "H");
-        Assertions.assertEquals(titleH.getCssValue("color"), "rgba(255, 0, 0, 1)");
+    @Test
+    public void matchLevelTest() {
+        driver.get("https://applitools.com/helloworld/?diff1");
+        eyes.check(Target.window()
+                .content(By.cssSelector("div.section:nth-child(1)"))
+                .layout(By.cssSelector("div.section:nth-child(2) > p:nth-child(1)")));
+    }
 
-        Assertions.assertEquals(titleD.isDisplayed(), true);
-        Assertions.assertEquals(titleD.getText(), "D");
-        Assertions.assertEquals(titleD.getCssValue("color"), "rgba(70, 0, 255, 1)");`
+    @Test
+    public void exampleTestCase() {
+        driver.get("https://example.com");
+        eyes.check(Target.window());
     }
 
     @AfterEach
     public void afterEach() {
+        eyes.close();
         driver.close();
+        driver.quit();
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        driver.quit();
+        TestResultsSummary testResultContainers = eyesRunner.getAllTestResults();
+        System.out.println(testResultContainers);
     }
 }
